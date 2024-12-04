328cfc46e213fb8c1b0d872cef5f21e461240bc2
DanBokete <150482588+DanBokete@users.noreply.github.com>
2024-12-04 10:06:06 +0000

    Add Members + changed img file name for duplicates

--- team.js
+++ team.js
@@ -42,15 +42,16 @@ const members = [
         firstName: "Marlon",
         lastName: "Ares",
         role: "Secretary",
-        description:
-            "Marlon manages meeting minutes.",
-        linkedIn: "https://www.linkedin.com/in/marlon-jesus-ares-milian-4b2a16203/",
+        description: "Marlon manages meeting minutes.",
+        linkedIn:
+            "https://www.linkedin.com/in/marlon-jesus-ares-milian-4b2a16203/",
     },
     {
         firstName: "Mikolaj",
         lastName: "Wujek",
         role: "Social Media Manager",
-        description: "Mikolaj created the ACM poster, and manages our social media platforms.",
+        description:
+            "Mikolaj created the ACM poster, and manages our social media platforms.",
         linkedIn: "https://www.linkedin.com/in/mikolaj-wujek-132354333/",
     },
     {
@@ -75,8 +76,31 @@ const members = [
         role: "OCM",
         description:
             "Pawel assisted with the development/managing of the website and online infrastructure.",
+        linkedIn:
+            "https://www.linkedin.com/in/pawe%C5%82-popkiewicz-b3b727251/",
+    },
+    {
+        firstName: "David",
+        lastName: "Shanahan",
+        role: "Vice Chair",
+        description: "David is a Masters student and Vice Chair.",
         linkedIn: null,
-    }
+    },
+    {
+        firstName: "Abigail Elizabeth",
+        lastName: "Joseph",
+        role: "OCM",
+        description: "Abigail is an OCM who is involved in the technical team",
+        linkedIn: null,
+    },
+    {
+        firstName: "George",
+        lastName: "Osemwengie",
+        role: "OCM",
+        description:
+            "George is an OCM who is involved in the organisation of the ACM event.",
+        linkedIn: null,
+    },
 ];
 
 const teamContainer = document.getElementById("teamContainer");
@@ -88,13 +112,17 @@ members.forEach((person) => {
     onclick="expandTeamMember(this)"
     >
     <img
-        src="images/${person.firstName.toLowerCase()}.jpg"
+        src="images/${person.firstName.toLowerCase()}_${person.lastName.toLowerCase()}.jpg"
         alt="An image of ${person.firstName} ${person.lastName}"
     />
     <h3>${person.firstName} ${person.lastName ? person.lastName : ""}</h3>
-    <p class="role">${person.role ? person.role : "Unsure"}</p>
+    <p class="role">${person.role ? person.role : "Acm Member"}</p>
     <p class="description">
-        ${person.description ? person.description : "Unsure"}
+        ${
+            person.description
+                ? person.description
+                : `${person.firstName} is apart of the team who contributes to the organisation of the event`
+        }
     </p>
     <a
         aria-label="link to linked in page"
